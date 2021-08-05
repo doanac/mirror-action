33fa94ece4dd802b7af81d5c9c4812a40b0463f0
Andy Doan <andy@foundries.io>
2021-08-05 16:06:31 -0500

    Add logic for syncing pull requests
    Signed-off-by: Andy Doan <andy@foundries.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,6 +38,10 @@ fi
 git remote add mirror "${REMOTE}"
 if [[ "${INPUT_PUSH_ALL_REFS}" != "false" ]]; then
     eval git push ${GIT_PUSH_ARGS} mirror "\"refs/remotes/origin/*:refs/heads/*\""
+    if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+        echo "Syncing PR ${GITHUB_REF}"
+	git push --force mirror HEAD:${GITHUB_REF}
+    fi
 else
     if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
         echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch" > /dev/stderr
